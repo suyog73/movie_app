719c9c080510fbb5a3366900a42d50ed2d698a5d
SUYOG PATIL <suyog732002@gmail.com>
2021-12-06 23:09:23 +0530

    camera issue

--- lib/screens/movie_create_screen.dart
+++ lib/screens/movie_create_screen.dart
@@ -352,6 +352,7 @@ class _MovieCreateScreenState extends State<MovieCreateScreen> {
                 InkWell(
                   onTap: () {
                     getImage(ImageSource.camera);
+                    Navigator.pop(context);
                   },
                   child: Container(
                     color: Colors.lightBlueAccent,
